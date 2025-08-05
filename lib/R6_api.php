e57f7faaec5352179ecc71131364454bcee13a0c
RVXTD <njg44@njit.edu>
2025-08-04 22:47:12 -0400

    Adding insert feature

--- lib/R6_api.php
+++ lib/R6_api.php
@@ -1,28 +1,52 @@
 <?php
 
-$curl = curl_init();
 
-curl_setopt_array($curl, [
-	CURLOPT_URL => "https://rainbow-six.p.rapidapi.com/general/pc/VG-Rahkwal",
-	CURLOPT_RETURNTRANSFER => true,
-	CURLOPT_ENCODING => "",
-	CURLOPT_MAXREDIRS => 10,
-	CURLOPT_TIMEOUT => 30,
-	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-	CURLOPT_CUSTOMREQUEST => "GET",
-	CURLOPT_HTTPHEADER => [
-		"x-rapidapi-host: rainbow-six.p.rapidapi.com",
-		"x-rapidapi-key: 55c3c19516mshccf881b1c1f1334p1f3dbajsn548aaa99584b"
-	],
-]);
+require(__DIR__ . "/R6_insert.php");
 
-$response = curl_exec($curl);
-$err = curl_error($curl);
+// Add back $db param when cache function created
+function fetch_act_data($actname, $kills, $deaths, $matches)
+{
+    $db = getDB();
 
-curl_close($curl);
+    $endpoint = "https://rainbow-six.p.rapidapi.com/general/pc/VG-Rahkwal";
+    $isRapidAPI = true;
+    $rapidAPIHost = "x-rapidapi-host: rainbow-six.p.rapidapi.com";
 
-if ($err) {
-	echo "cURL Error #:" . $err;
-} else {
-	echo $response;
+    $result = get($endpoint, "R6_API_KEY", $data, $isRapidAPI, $rapidAPIHost);
+
+    if ($result["status"] === 200 && isset($result["response"])) {
+        $decoded = json_decode($result["response"], true);
+
+        foreach ($decoded as $acccount => $r6) {
+            error_log("== Parsed Account Data ==");
+
+            $actname = $r6["name"] ?? '';
+            $kills = $r6["kills"] ?? '';
+            $deaths = $r6["deaths"] ?? '';
+            $matches = $r6["matches"] ?? '';
+            $is_api = 1;
+
+            error_log("name: $actname");
+            error_log("kills: $kills");
+            error_log("deaths: $deaths");
+            error_log("matches: $matches");
+
+            $activityFlags = [];
+
+
+            error_log("is_api: $is_api");
+
+            // Insert using the passed DB handle
+            insert_account($db, array_merge([
+                "name" => $actname,
+                "kills" => $kills,
+                "deaths" => $deaths,
+                "matches" => $matches,
+                "is_api" => $is_api, ], $activityFlags));
+        }
+
+        return $decoded;
+    }
+
+    return [];
 }
